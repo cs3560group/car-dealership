0b49b00c6616ca514d1e0c6c4079a6c1c9f4727e
noah <ojedanoah24@gmail.com>
2025-04-10 10:58:24 -0700

    adding the classes i deleted back
    my fault

---
+++ src/com/dealership/Inventory.java
@@ -0,0 +1,19 @@
+package com.dealership;
+
+public class Inventory {
+    private int totalStock;
+    private int thresholdLimit;
+
+    public Inventory(int totalStock, int thresholdLimit) {
+        this.totalStock = totalStock;
+        this.thresholdLimit = thresholdLimit;
+    }
+
+    public int getTotalStock() {
+        return totalStock;
+    }
+
+    public int getThresholdLimit() {
+        return thresholdLimit;
+    }
+}
