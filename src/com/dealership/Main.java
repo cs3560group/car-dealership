7ad60afdaf6b70de579b610c4f8243e18d119a7e
noah <ojedanoah24@gmail.com>
2025-04-10 11:20:20 -0700

    edits to main
    testing to see if the customer class / main can be ran

--- src/com/dealership/Main.java
+++ src/com/dealership/Main.java
@@ -6,5 +6,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Customer customer = new Customer("John", "1234567890", "JohnSDas@sdjiasj.com", "1234");
         customer.printCustomerInfo();
+
+        System.out.println("Testing");
     }
 }
