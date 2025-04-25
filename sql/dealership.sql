9a362b4de1cf28b9ff6f59cdec4bc9afd6aca77c
taibui97 <taibui97@outlook.com>
2025-04-25 14:39:00 -0700

    add db

--- sql/dealership.sql
+++ sql/dealership.sql
@@ -161,7 +161,7 @@ CREATE TABLE `users` (
   `password` varchar(50) DEFAULT NULL,
   `role` enum('admin','employee','manager','customer') NOT NULL DEFAULT 'employee',
   PRIMARY KEY (`userID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -170,7 +170,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'alice','casdca@asfdcas.com',NULL,'employee'),(2,'dasda','dasd@sdfsdfsd','sdfsdf','employee');
+INSERT INTO `users` VALUES (1,'alice','casdca@asfdcas.com',NULL,'employee'),(2,'dasda','dasd@sdfsdfsd','sdfsdf','employee'),(3,'asd','asdas','dasd','employee'),(4,'asd','asdas','dasd','employee'),(5,'admin','admin@dealership.com','123456','admin');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -217,4 +217,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-04-24  0:36:32
+-- Dump completed on 2025-04-25 14:36:23
